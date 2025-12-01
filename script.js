2364dc4509a657ad33c48b61b88e13ebf2a39d15
Saad Patel <psaad2988@gmail.com>
2025-12-01 15:40:42 +0530

    frontend ready

---
+++ script.js
@@ -0,0 +1,33 @@
+async function shortenURL() {
+    const longUrl = document.getElementById("urlInput").value;
+
+    if (!longUrl) {
+        alert("Please enter a URL!");
+        return;
+    }
+
+    const response = await fetch("https://fastapi-shortner-qr.onrender.com/shorten/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ long_url: longUrl })
+    });
+
+    if (!response.ok) {
+        alert("Error shortening URL");
+        return;
+    }
+
+    const data = await response.json();
+
+    // Show section
+    document.getElementById("result").classList.remove("hidden");
+
+    // Set short URL link
+    const shortLink = `https://fastapi-shortner-qr.onrender.com/${data.short_url}`;
+    document.getElementById("shortUrl").textContent = shortLink;
+    document.getElementById("shortUrl").href = shortLink;
+
+    // Set QR Code image
+    document.getElementById("qrImage").src =
+        `https://fastapi-shortner-qr.onrender.com/qr/${data.short_url}.png`;
+}
